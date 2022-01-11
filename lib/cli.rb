dd90d1a1599adbbf1bc44818b5f3a8c5a863605c
Jason Botesazan <jbotesazan@gmail.com>
2022-01-11 15:47:11 -0500

    commit tree eaf732d056367247986b779866f35a4f7f2ce759

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,6 +1,10 @@
-def initialize_app 
-    movies = ['2001: A Space Odyssey', 'The Godfather', 'Citizen Kane', 'Raiders of the Lost Ark', 'In the mood for love', 'Seven Samurai']
+require 'pry'
 
+def initialize_app
+    menu 
+end
+
+def menu
     puts "Welcome to Flatiron Movies"
     puts "Please choose an option:"
     puts "1. List all Movies"
@@ -11,19 +15,30 @@ def initialize_app
     user_input = gets.strip
 
     case user_input
+
     when "1"
+
+        movies = ['2001: A Space Odyssey', 'The Godfather', 'Citizen Kane', 'Raiders of the Lost Ark', 'In the mood for love', 'Seven Samurai']
+
         movies.each {|movie| puts movie }
         
     when "2"
 
     when "3"
-        puts create_ticket
+        def create_ticket
+            puts 'name'
+            name = gets.strip
+            puts 'Title'
+            title = gets.strip
+
+            t1 = Ticket.new(name, title)
+            binding.pry
+        end
         
     when "0" 
         puts "goodbye"
     else 
-        puts "sorry we don't have that option"
-        
+        # puts "sorry we don't have that option" 
     end 
 
 end 
---
+++ lib/ticket.rb
@@ -0,0 +1,9 @@
+class Ticket
+    attr_accessor :name, :title, :price
+
+    def initialize (name, title)
+        @name = name
+        @tile = title
+        @price = 5.00
+    end
+end
